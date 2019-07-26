9d96fd26826760b1748dff8d155250042567b6f1
Radu B <radu.buciuceanu@gmail.com>
2019-07-26 19:29:33 +0300

    Change PersonRepository.cpp.

--- src/Cmps.Data/src/Cmps.Data/Repositories/PersonRepository.cpp
+++ src/Cmps.Data/src/Cmps.Data/Repositories/PersonRepository.cpp
@@ -1,15 +1,9 @@
 #include "Cmps.Data/Repositories/PersonRepository.h"
 
-namespace Cmps
-{
-namespace Data
-{
-namespace Repositories
+namespace Cmps::Data::Repositories
 {
 const Person* PersonRepository::create(const Person* person)
 {
 	return person;
-}
-}
 }
 }
