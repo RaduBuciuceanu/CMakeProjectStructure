c5131a33cbfa515005902aa147cc2260fb73f018
Radu B <radu.buciuceanu@gmail.com>
2019-07-26 19:48:03 +0300

    Change Cmps::Data::Repositories::PersonRepository.

--- src/Cmps.Data/include/Cmps.Data/Repositories/PersonRepository.h
+++ src/Cmps.Data/include/Cmps.Data/Repositories/PersonRepository.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include "Cmps.Business/Repositories/IPersonRepository.h"
 
 namespace Cmps::Data::Repositories
@@ -8,6 +10,9 @@ using namespace Business;
 
 class PersonRepository : public IPersonRepository
 {
+private:
+	std::vector<const Person*> _persons;
+
 public:
 	const Person* create(const Person* person) override;
 };
--- src/Cmps.Data/src/Cmps.Data/Repositories/PersonRepository.cpp
+++ src/Cmps.Data/src/Cmps.Data/Repositories/PersonRepository.cpp
@@ -4,6 +4,7 @@ namespace Cmps::Data::Repositories
 {
 const Person* PersonRepository::create(const Person* person)
 {
+	_persons.push_back(person);
 	return person;
 }
 }
