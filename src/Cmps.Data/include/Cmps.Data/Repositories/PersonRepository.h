e4ac6c37244345637d1514c397f06d575f538519
Radu B <radu.buciuceanu@gmail.com>
2019-07-26 20:03:24 +0300

    Create Preprocessor.h for data layer.

--- src/Cmps.Data/include/Cmps.Data/Repositories/PersonRepository.h
+++ src/Cmps.Data/include/Cmps.Data/Repositories/PersonRepository.h
@@ -2,13 +2,14 @@
 
 #include <vector>
 
+#include <Cmps.Data/Preprocessor.h>
 #include "Cmps.Business/Repositories/IPersonRepository.h"
 
 namespace Cmps::Data::Repositories
 {
 using namespace Business;
 
-class PersonRepository : public IPersonRepository
+class PUBLIC PersonRepository : public IPersonRepository
 {
 private:
 	std::vector<const Person*> _persons;
