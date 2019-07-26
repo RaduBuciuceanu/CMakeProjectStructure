ea9aa42fcfa5b286045eb0fdbbfa915735c79a95
Radu B <radu.buciuceanu@gmail.com>
2019-07-26 19:53:57 +0300

    Change Program.cpp.

--- src/Cmps.Console/src/Cmps.Console/Program.cpp
+++ src/Cmps.Console/src/Cmps.Console/Program.cpp
@@ -1,8 +1,18 @@
-#include <Cmps.Business/Person.h>
+#include <iostream>
 
+#include <Cmps.Business/Person.h>
+#include <Cmps.Data/Repositories/PersonRepository.h>
+
+using namespace std;
 using namespace Cmps::Business;
+using namespace Cmps::Data::Repositories;
 
 int main()
 {
-	return 0;
+	auto repository = PersonRepository();
+	auto one = Person("Ghita", "Bobocel");
+
+	repository.create(&one);
+
+	return cin.get();
 }
